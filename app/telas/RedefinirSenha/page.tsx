3820239bce21058ef55c199039d8905447e1c563
BrunoSevero-188 <brunosevero188@gmail.com>
2026-05-31 20:03:27 -0400

    Configurando Os linksInfo

--- app/telas/RedefinirSenha/page.tsx
+++ app/telas/RedefinirSenha/page.tsx
@@ -56,7 +56,7 @@ export default function RedefinirSenha() {
             />
           </div>
 
-          <div className={styleLinkInfo.containerConjuntoLinks}>
+          <div className={styleLinkInfo.containerConjuntoInfo}>
             <LinkInfo
               modo="icone"
               icon="📧"
