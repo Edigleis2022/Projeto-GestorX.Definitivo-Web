43c313948cfad6569f7152489b0425c005392f6a
BrunoSevero-188 <brunosevero188@gmail.com>
2026-06-10 14:28:57 -0400

    Atualização

---
+++ app/telas/TelasInternas/slideBar/Estante/Monitoramento/Movimentação/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Page() {
+  return (
+    <main style={{ padding: 16 }}>
+      <h1 style={{ fontSize: 20, fontWeight: 700 }}>Movimentação</h1>
+      <p style={{ marginTop: 8 }}>
+        Página em manutenção.
+      </p>
+    </main>
+  );
+}
+
