e784ae77a42d7f7122716f1cab3492d0fa3be8e1
BrunoSevero-188 <brunosevero188@gmail.com>
2026-06-11 00:13:59 -0400

    Atualização

---
+++ app/telas/TelasInternas/slideBar/Estante/Monitoramento/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Page() {
+  return (
+    <main style={{ padding: 16 }}>
+      <h1 style={{ fontSize: 20, fontWeight: 700 }}>Monitoramento</h1>
+      <p style={{ marginTop: 8 }}>Selecione uma opção no menu para ver os detalhes.</p>
+    </main>
+  );
+}
